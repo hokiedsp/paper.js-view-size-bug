765b86dc88f8f4bfde6732e88b1adb04886ccbfb
Takeshi Ikuma <tikuma@gmail.com>
2020-01-14 17:07:51 -0600

    added resizing effect

--- index.js
+++ index.js
@@ -4,17 +4,37 @@
 // import paper from "paper/dist/paper-full.min";
 // const paper = require("../vendor/paper.js/src/export.js")
 
+$div = document.querySelector("div.container");
+
 paper.install(window);
+view = paper.view;
 
 // Only executed our code once the DOM is ready.
 window.onload = function() {
   // Get a reference to the canvas object
-  var canvas = document.querySelector("canvas.paper");
+  canvas = document.querySelector("canvas.paper");
   // Create an empty project and a view for the canvas:
   paper.setup(canvas);
 
-  var view = paper.view;
-  console.log(`view.size: ${view.size.toString()}`)
-  console.log(`view.viewSize: ${view.viewSize.toString()}`)
-  console.log(`view.pixelRatio: ${view.pixelRatio.toString()}`)
+  console.log("Initial view size:")
+  console.log(` view.size: ${view.size.toString()}`);
+  console.log(` view.viewSize: ${view.viewSize.toString()}`);
+  console.log(` view.pixelRatio: ${view.pixelRatio.toString()}`);
+
+  console.log("Resizing canvas by setting view.viewSize...")
+  paper.view.viewSize = [$div.offsetWidth, $div.offsetHeight];
+
+  console.log("New view size:")
+  console.log(` view.size: ${view.size.toString()}`);
+  console.log(` view.viewSize: ${view.viewSize.toString()}`);
+  console.log(` view.pixelRatio: ${view.pixelRatio.toString()}`);
+
+  var text = new this.paper.PointText({
+    content: "This text should be in the middle",
+    point: [500, 350],
+    justification: "right"
+  });
+
+  console.log("Text position:")
+  console.log(` text.point: ${text.point.toString()}`);
 };
